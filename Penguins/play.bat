af608afb7820791f7a48fc9a5580b0f98f8c228a
Arkadiusz Koszewski <est@onet.com.pl>
2017-01-13 11:01:00 +0000

    play.bat created online with Bitbucket

---
+++ Penguins/play.bat
@@ -0,0 +1,9 @@
+1 phase=placement penguins=3 board.txt board2.txt
+2 phase=placement penguins=3 board2.txt board3.txt
+1 phase=placement penguins=3 board3.txt board4.txt
+2 phase=placement penguins=3 board4.txt board5.txt
+1 phase=placement penguins=3 board5.txt board6.txt
+2 phase=placement penguins=3 board6.txt board7.txt
+
+1 phase=movement board7.txt board10.txt
+2 phase=movement board10.txt board11.txt
